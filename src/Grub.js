16558c9ddc85901ac67c2dfb5593ef84aa9350a1
Jin Bok <devJin86@gmail.com>
2016-03-07 11:28:22 -0800

    Complete implementing Grub class. Passing all specs

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function() {
+  var food = this.food;
+  console.log('eatting ' + this.food);
+  this.food = null;
+  return food;
+}
